c60703d3b44a782165ce3087394b922d2f1b027d
arma73 <git@arma73.tech>
2024-05-03 22:30:17 +0400

    fix: move task one to dir task-first

--- tasks/task-first/jest.config.ts
+++ tasks/task-first/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
   displayName: 'task-first',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../coverage/tasks',
+  coverageDirectory: '../../coverage/tasks/task-first',
 };
